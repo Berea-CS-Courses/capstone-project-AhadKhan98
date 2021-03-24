89a484d4b857677fb5e2cd81abec42888ce5a385
AhadKhan98 <ahad_zai@berea.edu>
2021-03-24 14:57:43 +0500

    establish connection between frontend and server

--- HelpZen/react-frontend/src/App.js
+++ HelpZen/react-frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import axios from "axios";
 
 function App() {
+  const getResponse = () => {
+    axios
+      .get("http://localhost:8000/")
+      .then((response) => {
+        alert("Got response from server!");
+        setServerMessage(response.data);
+      })
+      .catch((error) => {
+        alert("Failed to get response from server!");
+        setServerMessage(error.message);
+      });
+  };
+  const [serverMessage, setServerMessage] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2>React frontend running for HelpZen</h2>
+      Press button to connect to backend server:{" "}
+      <button onClick={getResponse}>Connect</button>
+      <p>{serverMessage}</p>
     </div>
   );
 }
