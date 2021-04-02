90e6752219f028cf67a4f3b95e05b342bb2f4029
AhadKhan98 <ahad_zai@berea.edu>
2021-04-02 19:17:05 +0500

    fix styling on the logged out user page

--- HelpZen/react-frontend/src/components/HomeComponent/LoggedOut/index.js
+++ HelpZen/react-frontend/src/components/HomeComponent/LoggedOut/index.js
@@ -36,7 +36,7 @@ function LoggedOut({ user }) {
         <div className="home--loggedOut--contentArea--block2">
           <h1>How it Works</h1>
           <div className="home--loggedOut--contentArea--block2--steps">
-            <div className="home--loggedOut--contentArea--block2--steps--1">
+            <div className="home--loggedOut--contentArea--block2--steps--container">
               <PersonIcon className="home--loggedOut--contentArea--block2--icon" />
               <h2 className="home--loggedOut--contentArea--block2--stepTitle">
                 STEP 1
@@ -46,43 +46,46 @@ function LoggedOut({ user }) {
               </h4>
             </div>
             <DoubleArrowIcon />
-            <div className="home--loggedOut--contentArea--block2--steps--1">
+            <div className="home--loggedOut--contentArea--block2--steps--container">
               <PhoneAndroidIcon className="home--loggedOut--contentArea--block2--icon" />
               <h2 className="home--loggedOut--contentArea--block2--stepTitle">
                 STEP 2
               </h2>
               <h4 className="home--loggedOut--contentArea--block2--stepDescription">
-                Create a new account or login to an existing account.
+                Select the technology that you need help with or wish to provide
+                help with.
               </h4>
             </div>
             <DoubleArrowIcon />
-            <div className="home--loggedOut--contentArea--block2--steps--1">
+            <div className="home--loggedOut--contentArea--block2--steps--container">
               <CodeIcon className="home--loggedOut--contentArea--block2--icon" />
               <h2 className="home--loggedOut--contentArea--block2--stepTitle">
                 STEP 3
               </h2>
               <h4 className="home--loggedOut--contentArea--block2--stepDescription">
-                Create a new account or login to an existing account.
+                Select the programming language and describe your problem if you
+                are seeking help.
               </h4>
             </div>
             <DoubleArrowIcon />
-            <div className="home--loggedOut--contentArea--block2--steps--1">
+            <div className="home--loggedOut--contentArea--block2--steps--container">
               <TimerIcon className="home--loggedOut--contentArea--block2--icon" />
               <h2 className="home--loggedOut--contentArea--block2--stepTitle">
                 STEP 4
               </h2>
               <h4 className="home--loggedOut--contentArea--block2--stepDescription">
-                Create a new account or login to an existing account.
+                Wait for the system to match you with someone.
               </h4>
             </div>
             <DoubleArrowIcon />
-            <div className="home--loggedOut--contentArea--block2--steps--1">
+            <div className="home--loggedOut--contentArea--block2--steps--container">
               <ChatIcon className="home--loggedOut--contentArea--block2--icon" />
               <h2 className="home--loggedOut--contentArea--block2--stepTitle">
                 STEP 5
               </h2>
               <h4 className="home--loggedOut--contentArea--block2--stepDescription">
-                Create a new account or login to an existing account.
+                Start chatting with the matched user and try to come up with a
+                solution to the problem together.
               </h4>
             </div>
           </div>
