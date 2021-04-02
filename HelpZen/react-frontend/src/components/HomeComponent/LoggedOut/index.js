ebc4e82f0ad7f99c034f7cd5dac7583e59c46433
AhadKhan98 <ahad_zai@berea.edu>
2021-04-02 18:44:43 +0500

    create block 2 and add icons for steps

--- HelpZen/react-frontend/src/components/HomeComponent/LoggedOut/index.js
+++ HelpZen/react-frontend/src/components/HomeComponent/LoggedOut/index.js
@@ -3,6 +3,12 @@ import React from "react";
 import "./styles.css";
 import firstblockImg from "../../../assets/images/firstblock.png";
 import NavBar from "../../NavBar";
+import PersonIcon from "@material-ui/icons/Person";
+import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
+import CodeIcon from "@material-ui/icons/Code";
+import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
+import TimerIcon from "@material-ui/icons/Timer";
+import ChatIcon from "@material-ui/icons/Chat";
 
 function LoggedOut({ user }) {
   return (
@@ -11,10 +17,13 @@ function LoggedOut({ user }) {
       <div className="home--loggedOut--contentArea">
         <div className="home--loggedOut--contentArea--block1">
           <div className="home--loggedOut--contentArea--block1--leftItems">
-            <h1>The Real-Time Helping Platform</h1>
-            <h4>
+            <h1 className="home--loggedOut--contentArea--block1--leftItems--title">
+              The Real-Time Helping Platform
+            </h1>
+            <h4 className="home--loggedOut--contentArea--block1--leftItems--paragraph">
               Helpzen is an online community where programmers can help out each
-              other.
+              other. Consisting of a wide range of technologies and programming
+              languages, you can find help catered to fit your needs.
             </h4>
           </div>
           <div className="home--loggedOut--contentArea--block1--rightItems">
@@ -22,6 +31,60 @@ function LoggedOut({ user }) {
               className="home--loggedOut--contentArea--block1--rightItems--image"
               src={firstblockImg}
             />
+          </div>
+        </div>
+        <div className="home--loggedOut--contentArea--block2">
+          <h1>How it Works</h1>
+          <div className="home--loggedOut--contentArea--block2--steps">
+            <div className="home--loggedOut--contentArea--block2--steps--1">
+              <PersonIcon className="home--loggedOut--contentArea--block2--icon" />
+              <h2 className="home--loggedOut--contentArea--block2--stepTitle">
+                STEP 1
+              </h2>
+              <h4 className="home--loggedOut--contentArea--block2--stepDescription">
+                Create a new account or login to an existing account.
+              </h4>
+            </div>
+            <DoubleArrowIcon />
+            <div className="home--loggedOut--contentArea--block2--steps--1">
+              <PhoneAndroidIcon className="home--loggedOut--contentArea--block2--icon" />
+              <h2 className="home--loggedOut--contentArea--block2--stepTitle">
+                STEP 2
+              </h2>
+              <h4 className="home--loggedOut--contentArea--block2--stepDescription">
+                Create a new account or login to an existing account.
+              </h4>
+            </div>
+            <DoubleArrowIcon />
+            <div className="home--loggedOut--contentArea--block2--steps--1">
+              <CodeIcon className="home--loggedOut--contentArea--block2--icon" />
+              <h2 className="home--loggedOut--contentArea--block2--stepTitle">
+                STEP 3
+              </h2>
+              <h4 className="home--loggedOut--contentArea--block2--stepDescription">
+                Create a new account or login to an existing account.
+              </h4>
+            </div>
+            <DoubleArrowIcon />
+            <div className="home--loggedOut--contentArea--block2--steps--1">
+              <TimerIcon className="home--loggedOut--contentArea--block2--icon" />
+              <h2 className="home--loggedOut--contentArea--block2--stepTitle">
+                STEP 4
+              </h2>
+              <h4 className="home--loggedOut--contentArea--block2--stepDescription">
+                Create a new account or login to an existing account.
+              </h4>
+            </div>
+            <DoubleArrowIcon />
+            <div className="home--loggedOut--contentArea--block2--steps--1">
+              <ChatIcon className="home--loggedOut--contentArea--block2--icon" />
+              <h2 className="home--loggedOut--contentArea--block2--stepTitle">
+                STEP 5
+              </h2>
+              <h4 className="home--loggedOut--contentArea--block2--stepDescription">
+                Create a new account or login to an existing account.
+              </h4>
+            </div>
           </div>
         </div>
       </div>
