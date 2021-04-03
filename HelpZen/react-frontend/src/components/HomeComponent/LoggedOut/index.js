6cb7d5c7eb880d0d7b72ed8bd0cdfe266f005525
AhadKhan98 <ahad_zai@berea.edu>
2021-04-03 13:36:33 +0500

    fix styling issues on first and second block

--- HelpZen/react-frontend/src/components/HomeComponent/LoggedOut/index.js
+++ HelpZen/react-frontend/src/components/HomeComponent/LoggedOut/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Button } from "@material-ui/core";
 import "./styles.css";
 import firstblockImg from "../../../assets/images/firstblock.png";
 import NavBar from "../../NavBar";
@@ -34,10 +34,12 @@ function LoggedOut({ user }) {
           </div>
         </div>
         <div className="home--loggedOut--contentArea--block2">
-          <h1>How it Works</h1>
+          <h1 className="home--loggedOut--contentArea--block2--header">
+            How it Works
+          </h1>
           <div className="home--loggedOut--contentArea--block2--steps">
             <div className="home--loggedOut--contentArea--block2--steps--container">
-              <PersonIcon className="home--loggedOut--contentArea--block2--icon" />
+              <PersonIcon className="home--loggedOut--contentArea--block2--icon icon--person" />
               <h2 className="home--loggedOut--contentArea--block2--stepTitle">
                 STEP 1
               </h2>
@@ -47,7 +49,7 @@ function LoggedOut({ user }) {
             </div>
             <DoubleArrowIcon />
             <div className="home--loggedOut--contentArea--block2--steps--container">
-              <PhoneAndroidIcon className="home--loggedOut--contentArea--block2--icon" />
+              <PhoneAndroidIcon className="home--loggedOut--contentArea--block2--icon icon--phone" />
               <h2 className="home--loggedOut--contentArea--block2--stepTitle">
                 STEP 2
               </h2>
@@ -58,7 +60,7 @@ function LoggedOut({ user }) {
             </div>
             <DoubleArrowIcon />
             <div className="home--loggedOut--contentArea--block2--steps--container">
-              <CodeIcon className="home--loggedOut--contentArea--block2--icon" />
+              <CodeIcon className="home--loggedOut--contentArea--block2--icon icon--code" />
               <h2 className="home--loggedOut--contentArea--block2--stepTitle">
                 STEP 3
               </h2>
@@ -69,7 +71,7 @@ function LoggedOut({ user }) {
             </div>
             <DoubleArrowIcon />
             <div className="home--loggedOut--contentArea--block2--steps--container">
-              <TimerIcon className="home--loggedOut--contentArea--block2--icon" />
+              <TimerIcon className="home--loggedOut--contentArea--block2--icon icon--clock" />
               <h2 className="home--loggedOut--contentArea--block2--stepTitle">
                 STEP 4
               </h2>
@@ -79,7 +81,7 @@ function LoggedOut({ user }) {
             </div>
             <DoubleArrowIcon />
             <div className="home--loggedOut--contentArea--block2--steps--container">
-              <ChatIcon className="home--loggedOut--contentArea--block2--icon" />
+              <ChatIcon className="home--loggedOut--contentArea--block2--icon icon--chat" />
               <h2 className="home--loggedOut--contentArea--block2--stepTitle">
                 STEP 5
               </h2>
@@ -89,7 +91,17 @@ function LoggedOut({ user }) {
               </h4>
             </div>
           </div>
+          <div className="home--loggedOut--contentArea--buttonContainer">
+            <Button
+              className="home--loggedOut--contentArea--joinButton"
+              variant="outlined"
+            >
+              Join The Community
+            </Button>
+          </div>
         </div>
+
+        <div className="home--loggedOut--contentArea--footer"></div>
       </div>
     </div>
   );
