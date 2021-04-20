89de9d2c4255c9e1f6c4a54cd7155a7bd7820aff
Ahad Zai <ahad_zai@berea.edu>
2021-04-20 21:51:15 +0500

    add comments to views

--- HelpZen/react-frontend/src/views/ChatView.js
+++ HelpZen/react-frontend/src/views/ChatView.js
@@ -1,3 +1,7 @@
+/**
+ * Gets necessary data to render ChatComponent
+ * Renders ChatComponent and sends required data
+ */
 import React from "react";
 
 import ChatComponent from "../components/ChatComponent";
