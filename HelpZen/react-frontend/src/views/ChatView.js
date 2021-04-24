bdd0ae1cc2837c063c87f4ea7b69d9f4cac12e51
Ahad Zai <ahad_zai@berea.edu>
2021-04-25 03:33:41 +0500

    add comments to ChatView

--- HelpZen/react-frontend/src/views/ChatView.js
+++ HelpZen/react-frontend/src/views/ChatView.js
@@ -9,14 +9,21 @@ import {deleteMatchById} from '../api';
 import ChatComponent from "../components/ChatComponent";
 
 function ChatView(props) {
-  const session = props.location.session;
+  const session = props.location.session; // Gets session object form data being passed via props
 
   useEffect(() => {
+    /**
+     * Deletes both of the match objects from MongoDB
+     */
     const deleteMatchesFromDb = () => {
       const matchObject1 = session.currentMatchQuery;
       const matchObject2 = session.matchFound;
-      deleteMatchById({matchId: matchObject1._id}).then((res) => console.log("MatchObject1 Deletion Status: ", res.data))
-      deleteMatchById({matchId: matchObject2._id}).then((res) => console.log("MatchObject1 Deletion Status: ", res.data))
+      
+      deleteMatchById({matchId: matchObject1._id}) // Calls API to delete the match object and waits for a response
+      .then((res) => console.log("MatchObject1 Deletion Status: ", res.data));
+
+      deleteMatchById({matchId: matchObject2._id}) // Calls API to delete the match object and waits for a response
+      .then((res) => console.log("MatchObject1 Deletion Status: ", res.data));
     }
     deleteMatchesFromDb();
   }, [session.currentMatchQuery, session.matchFound])
