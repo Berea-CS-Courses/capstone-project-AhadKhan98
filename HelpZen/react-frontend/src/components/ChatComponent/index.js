e1b5b422b1f028bab91b2c380fec46ff9f14dfa7
Ahad Zai <ahad_zai@berea.edu>
2021-04-27 08:46:48 +0500

    render member and tech stack data from props in chatview

--- HelpZen/react-frontend/src/components/ChatComponent/index.js
+++ HelpZen/react-frontend/src/components/ChatComponent/index.js
@@ -2,7 +2,7 @@
  * Renders UI for chat room
  * Restricts access to only users that have been matched
  */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "../Footer";
 import NavBar from "../NavBar";
 import {
@@ -15,9 +15,81 @@ import {
 import Message from "./Message";
 import "./styles.css";
 
+import { getUserById } from "../../api";
+
 function ChatComponent({ user, session }) {
-  console.log("User Object", user);
-  console.log("Session Object", session);
+  const [helpee, setHelpee] = useState(null);
+  const [helper, setHelper] = useState(null);
+
+  useEffect(() => {
+    const setHelpeeAndHelperToState = () => {
+      getUserById(session.currentMatchQuery.userId).then((response) => {
+        if (session.currentMatchQuery.userStatus === "helpee") {
+          setHelpee(response.data);
+        } else if (session.currentMatchQuery.userStatus === "helper") {
+          setHelper(response.data);
+        }
+      });
+      getUserById(session.matchFound.userId).then((response) => {
+        if (session.matchFound.userStatus === "helpee") {
+          setHelpee(response.data);
+        } else if (session.matchFound.userStatus === "helper") {
+          setHelper(response.data);
+        }
+      });
+    };
+    setHelpeeAndHelperToState();
+  }, []);
+
+  const renderTechnologyAsText = (technology) => {
+    switch (technology) {
+      case "ai":
+        return <>Artifical Intelligence</>;
+      case "datasci":
+        return <>Data Science</>;
+      case "databases":
+        return <>Databases</>;
+      case "Desktop":
+        return <>Desktop Development</>;
+      case "web":
+        return <>Web Development</>;
+      case "scripting":
+        return <>Scripting</>;
+      case "mobile":
+        return <>Mobile Development</>;
+      case "game":
+        return <>Game Development</>;
+    }
+  };
+
+  const renderLanguageAsText = (language) => {
+    switch (language) {
+      case "c":
+        return <>C</>;
+      case "cpp":
+        return <>C++</>;
+      case "csharp":
+        return <>C#</>;
+      case "golang":
+        return <>Go</>;
+      case "python":
+        return <>Python</>;
+      case "php":
+        return <>PHP</>;
+      case "javascript":
+        return <>JavaScript</>;
+      case "java":
+        return <>Java</>;
+      case "rlang":
+        return <>R</>;
+      case "ruby":
+        return <>Ruby</>;
+      case "sql":
+        return <>SQL</>;
+      case "swift":
+        return <>Swift</>;
+    }
+  };
 
   return (
     <div className="chat-component">
@@ -41,13 +113,13 @@ function ChatComponent({ user, session }) {
                   className="chat-component--body--left--members--bodytext"
                   variant="body1"
                 >
-                  Helper: Name
+                  Helper: {helper?.firstName}
                 </Typography>
                 <Typography
                   className="chat-component--body--left--members--bodytext"
                   variant="body1"
                 >
-                  Helpee: Name
+                  Helpee: {helpee?.firstName}
                 </Typography>
               </div>
               <div className="chat-component--body--left--technologies">
@@ -58,13 +130,27 @@ function ChatComponent({ user, session }) {
                   className="chat-component--body--left--technologies--bodytext"
                   variant="body1"
                 >
-                  Technology: Name
+                  Technology:{" "}
+                  {
+                    // Renders full form of the helpee's technology selection (e.g: datasci => Data Science)
+                    session.currentMatchQuery.userStatus === "helpee"
+                      ? renderTechnologyAsText(
+                          session.currentMatchQuery.technology
+                        )
+                      : renderTechnologyAsText(session.matchFound.technology)
+                  }
                 </Typography>
                 <Typography
                   className="chat-component--body--left--technologies--bodytext"
                   variant="body1"
                 >
-                  Language: Name
+                  Language:{" "}
+                  {
+                    // Renders full form of the helpee's language selection (e.g: csharp => C#)
+                    session.currentMatchQuery.userStatus === "helpee"
+                      ? renderLanguageAsText(session.currentMatchQuery.language)
+                      : renderLanguageAsText(session.matchFound.language)
+                  }
                 </Typography>
               </div>
             </div>
